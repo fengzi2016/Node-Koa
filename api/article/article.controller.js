2a41dfbb9116dce7825808b63fde688dab06b797
fengzi2016 <1820166258@qq.com>
2017-12-08 23:13:25 +0800

    'change'

--- api/article/article.controller.js
+++ api/article/article.controller.js
@@ -3,15 +3,20 @@ const _ = require('lodash');
 const path = require('path');
 const URL = require('url');
 
-//前台获取
+//前台，后台获取
 
-exports.getArticle= async (ctx,next)=>{
+exports.getAllArticle = async (ctx,next)=>{
+
+}
+exports.getOneArticle = async(ctx,next)=>{
 
 }
 exports.addArticle = async(ctx,next)=>{
 
 }
+exports.deleteArticle = async(ctx,next)=>{
+
+}
 
 
 
-//后台管理获取
